5ded82cd73326db3071b311b414d4d70450b506a
ArthurOReis <121455711+ArthurOReis@users.noreply.github.com>
2024-10-04 09:09:06 -0300

    Adicionando struct 'Estatistica'

--- main.c
+++ main.c
@@ -2,11 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct
+{
+    float temp_minima, temp_media, temp_maxima;
+} Estatisticas;
+
+Estatisticas calcula_estatisticas(float temperaturas[], int tamanho){
+
+    float soma = 0.0;
+    float menor;
+    float maior;
+
+    for (int i = tamanho; i != 0; i--)
+    {
+        
+        soma += temperaturas[i];
+    }
+    
+
+}
+
 int main(){
     FILE *arquivo = fopen("texto.txt", "r");
     char buffer[255];
     float temperaturas[20];
-    int i = 0;
     char numeroChar[4];
     float numero;
 
@@ -14,6 +33,7 @@ int main(){
         printf("Arquivo não pode ser aberto");
     } else {
 
+        int i = 0;
         while (fgets(buffer, 255, arquivo) != NULL){
             char *tempLugar = strchr(buffer, ';');
             strcpy(numeroChar, tempLugar + 1);
